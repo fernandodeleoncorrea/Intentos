ddf26f45d6472020fba22f8d281b09a594efa55d
fernandodeleoncorrea@gmail.com <fernandodeleoncorrea@gmail.com>
2020-10-15 12:47:35 -0300

    llamada y mail

--- app/src/main/java/com/example/intentos/DetalleContacto.java
+++ app/src/main/java/com/example/intentos/DetalleContacto.java
@@ -2,13 +2,48 @@ package com.example.intentos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class DetalleContacto extends AppCompatActivity {
+
+    private TextView tvNombre;
+    private TextView tvTelefono;
+    private TextView tvEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalle_contacto);
+
+        Bundle parametros = getIntent().getExtras();
+        String nombre = parametros.getString("NOMBRE");
+        String telefono = parametros.getString("TELEFONO");
+        String email = parametros.getString("EMAIL");
+
+         tvNombre = (TextView) (findViewById(R.id.tvNombre));
+         tvTelefono = (TextView) (findViewById(R.id.tvTelefono));
+         tvEmail = (TextView) (findViewById(R.id.tvEmail));
+
+        tvNombre.setText(nombre);
+        tvTelefono.setText(telefono);
+        tvEmail.setText(email);
+    }
+
+    public void llamar(View v){
+        String telefono= tvTelefono.getText().toString();
+        startActivity(new Intent(Intent.ACTION_CALL, Uri.parse("tel:"+ telefono)));
+    }
+
+    public void enviarcorreo(View v){
+        String email= tvEmail.getText().toString();
+        Intent emailIntent = new Intent((Intent.ACTION_SEND));
+        emailIntent.setData(Uri.parse("mailto:" ));
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, email);
+        emailIntent.setType("message/rfc822");
+        startActivity(Intent.createChooser(emailIntent, "Email"));
     }
 }
--- app/src/main/java/com/example/intentos/MainActivity.java
+++ app/src/main/java/com/example/intentos/MainActivity.java
@@ -2,13 +2,51 @@ package com.example.intentos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<Contacto> contactos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        contactos = new ArrayList<Contacto>();
+        contactos.add(new Contacto("Pepe Suarez", "1111111", "pepe@pepe.com"));
+        contactos.add(new Contacto("Juan Peres", "2222222","juan@juan.com"));
+        contactos.add(new Contacto("Laura Rosa", "333333", "laura@laura.com"));
+        contactos.add(new Contacto("Tuna Mate", "444444","tuna@tuna.com"));
+        contactos.add(new Contacto("papel Fernandez", "55555", "papel@papel.com"));
+
+        ArrayList<String> nombresContacto = new ArrayList<>();
+
+        for (Contacto contacto : contactos) {
+            nombresContacto.add(contacto.getNombre());
+        }
+
+        ListView lstContactos = (ListView) findViewById(R.id.lstContactos);
+        lstContactos.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,nombresContacto));
+
+        lstContactos.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //intent Explicito
+                Intent intent = new Intent(MainActivity.this, DetalleContacto.class);
+                intent.putExtra("NOMBRE", contactos.get(position).getNombre());
+                intent.putExtra("TELEFONO", contactos.get(position).getTelefono() );
+                intent.putExtra("EMAIL", contactos.get(position).getEmail() );
+                startActivity(intent);
+            }
+        });
     }
+
 }
